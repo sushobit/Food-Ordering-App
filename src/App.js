5dabd3da2f7cc5b9424be53b1a0ca07be71dda2c
Sushobhit Dhara <sushobitdhara@gmail.com>
2025-04-08 15:53:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,42 @@ import OrderFormPage from './Pages/OrderFormPage';
 import OrderOnlinePage from './Pages/OrderOnlinePage';
 import Footer from './Components/Footer';
 import PageLoader from './Components/PageLoader';
+import Blog from './Pages/Blog';
+import ContactUs from './Pages/ContactPage';
+import './App.css';
 
 const App = () => {
+  const [cursorPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    const cursor = document.querySelector('.cursor-animation');
+    if (!cursor) return;
+
+    const moveCursor = e => {
+      cursor.style.left = e.pageX + 'px';
+      cursor.style.top = e.pageY + 'px';
+    };
+
+    document.addEventListener('mousemove', moveCursor);
+
+    return () => {
+      document.removeEventListener('mousemove', moveCursor);
+    };
+  }, []);
   return (
     <Router>
+       {/* Cursor Animation */}
+       <div style={{
+          position: 'absolute',
+          width: '50px',
+          height: '50px',
+          backgroundColor: 'rgba(0, 0, 0, 0.2)',
+          borderRadius: '50%',
+          transform: 'translate(-50%, -50%)',
+          pointerEvents: 'none',
+          left: cursorPosition.x,
+          top: cursorPosition.y,
+          transition: 'left 0.1s ease-out, top 0.1s ease-out',
+        }} className="cursor-animation"></div>
       <Header />
       <PageLoader>
         <Routes>
@@ -20,6 +52,8 @@ const App = () => {
           <Route path="/cart" element={<OrderPage />} />
           <Route path="/orderonline" element={<OrderOnlinePage />} />
           <Route path="/order-form" element={<OrderFormPage />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contactus" element={<ContactUs />} />
         </Routes>
       </PageLoader>
       <Footer />
