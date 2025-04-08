8391f0700e977194ae3f7433f190f4fb75cb191c
Sushobhit Dhara <sushobitdhara@gmail.com>
2025-04-08 15:42:33 +0530

    Update HeroSection.js

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HeroSection.css'; 
 import { Tooltip } from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const HeroSection = () => {
   return (
@@ -9,7 +10,7 @@ const HeroSection = () => {
         <h1 className="hero-title">Savor Every Bite with Fast Delivery!</h1>
         <p className="hero-subtitle">Discover delicious meals delivered right to your door.</p>
         <a href='/orderonline'>
-        <Tooltip title="ORDER NOW"><button className="hero-button">Order Now</button></Tooltip>
+        <Tooltip title="ORDER NOW"><button className="hero-button">VIEW ALL MENU <ArrowForwardIcon sx={{ ml: 1, fontSize: '1.3rem' }} /></button></Tooltip>
         </a>
       </div>
     </div>
